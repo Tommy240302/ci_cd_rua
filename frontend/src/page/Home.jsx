d3abce1b9a563f2409040ff70fca8c389ae6e303
Tommy240302 <laikhacminhquang24032002@gmail.com>
2024-06-21 08:04:50 +0700

    update - home

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className="mt-[60px] w-full">
        <img className="w-full h-full" loading='lazy' src="/study.jpg" alt=""/>
        {test}
-       build ci/cd thành công + Quang
+       build ci/cd thành công + Quang dep trai
     </div>
   )
 }
